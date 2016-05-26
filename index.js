9e437508ebcfaff3ffc10b80d8075904848e9108
amjadafanah <amjadafanah@users.noreply.github.com>
2016-05-26 11:30:47 -0700

    Update to TEST

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World DEV\n');
+  res.send('Hello World TEST\n');
 });
 
 app.listen(PORT)
